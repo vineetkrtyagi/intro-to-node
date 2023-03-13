081244be72d58cb543a4858beef270596a4c9c59
vineetkrtyagi <75693395+vineetkrtyagi@users.noreply.github.com>
2023-03-13 22:05:19 +0530

    first commit in node first project

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes= require("superheroes");
+var mySuperhero= superheroes.random();
+
+var supervillain= require("supervillains");
+var mySupervillain= supervillain.random();
+
+console.log(mySuperhero);
+console.log(mySupervillain);
